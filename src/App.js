d6168be4e9c7de97d28ec3ab5201f960385cf4ae
jimcwx <jimcwx@gmail.com>
2021-07-09 11:37:11 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { makeStyles } from '@material-ui/core/styles'
+import { 
+  Table,
+  TableCell, 
+  TableBody,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper
+} from '@material-ui/core/'
+
+const useStyles = makeStyles({
+  table: {
+    minWidth:650,
+  }
+})
+
+function createData(name,calories,fat,carbs,protein) {
+  return { name, calories, fat, carbs, protein}
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159,6.0,24,4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37,4.3),
+  createData('Eclair', 262, 16.0,24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>HELLO</h1>
     </div>
   );
 }
